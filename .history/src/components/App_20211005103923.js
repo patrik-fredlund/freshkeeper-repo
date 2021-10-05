0a9987b672a2916a4fdee695d9b727065d9ceccd
patrik-fredlund <patrikfredlunds@gmail.com>
2021-10-05 11:26:26 +0200

    :sparkles: added firebase and signup

---
+++ .history/src/components/App_20211005103923.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>freshkeeper</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/components/App_20211005111635.js
@@ -0,0 +1,8 @@
+import '../App.css';
+import Signup from './Signup';
+
+function App() {
+  return <h1>test</h1>;
+}
+
+export default App;
